788ff43bf3d980a0fa251f8e11b57b2d0e6a1266
Raehale <alyssarae98ha@gmail.com>
2024-10-20 20:41:24 -0600

    made login buttons have one event listener

--- index.js
+++ index.js
@@ -64,6 +64,11 @@ const signUpForm = document.getElementById("sign-up-form")
 const signInForm = document.getElementById("sign-in-form")
 let isValid = false
 
+document.querySelectorAll(".login-btn").forEach(submitBtn => {
+    submitBtn.addEventListener("click", () => {
+        displayGenericModal(`${submitBtn.dataset.modalType}`)
+    })
+})
 signUpBtn.addEventListener("click", () => {
     displayGenericModal("sign-up")
 })
